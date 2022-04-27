a3265c5c20675d1eeb242480a478418f7f5c5a96
spriestman52 <spriestman52@gmail.com>
2022-04-27 19:06:25 -0400

    fixed buttons and header

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$('#aboutBtn').click(function(e){
+    var $target = $('html, #aboutme');
+    $target.animate({scrollTop: 1015}, 600);
+});
+
+$('#experienceBtn').click(function(e){
+    var $target = $('html, #experience');
+    $target.animate({scrollTop: 1475}, 600);
+});
+
+$('#educationBtn').click(function(e){
+    var $target = $('html, #education');
+    $target.animate({scrollTop: 1640}, 600);
+});
+
+$('#portfolioBtn').click(function(e){
+    var $target = $('html, #portfolio');
+    $target.animate({scrollTop: 1950}, 600);
+});
+
+$('#contactBtn').click(function(e){
+    var $target = $('html, #contact');
+    $target.animate({scrollTop: 5000}, 600);
+});
