fdd86069cd2f5ef83080e6b937bb5b1a83d7b1fe
spriestman52 <104237860+spriestman52@users.noreply.github.com>
2022-04-27 22:30:56 -0400

    fixed buttons kinda

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 $('#aboutBtn').click(function(e){
     var $target = $('html, #aboutme');
-    $target.animate({scrollTop: 1015}, 600);
+    $target.animate({scrollTop: 900}, 600);
 });
 
 $('#experienceBtn').click(function(e){
     var $target = $('html, #experience');
-    $target.animate({scrollTop: 1475}, 600);
+    $target.animate({scrollTop: 1361}, 600);
 });
 
 $('#educationBtn').click(function(e){
     var $target = $('html, #education');
-    $target.animate({scrollTop: 1640}, 600);
+    $target.animate({scrollTop: 1525}, 600);
 });
 
 $('#portfolioBtn').click(function(e){
     var $target = $('html, #portfolio');
-    $target.animate({scrollTop: 1950}, 600);
+    $target.animate({scrollTop: 1835}, 600);
 });
 
 $('#contactBtn').click(function(e){
